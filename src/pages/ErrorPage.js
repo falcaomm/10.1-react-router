714f09b684e4ed4e8b4634d3259ff81c48f7c726
falcaomm <manu.m.falcao@gmail.com>
2023-01-23 21:11:05 -0300

    ao vivo react router

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+
+export default function ErrorPage() {
+    return (
+        <>
+            <h1>Página de erro</h1>
+        </>
+    );
+}
+
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,12 +1,15 @@
+import { useParams } from "react-router-dom";
 import Header from "../components/Header";
 import { MainContainer } from "../style";
 
 function ProfilePage() {
+  const {name} = useParams()
 
   return (
     <MainContainer>
-      <Header />
-      <h1>Página acerca do usuário</h1>
+      <Header 
+      />
+      <h1>Página da {name}</h1>
     </MainContainer>
   );
 }
